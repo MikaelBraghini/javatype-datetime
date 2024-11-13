58f9eb2c2f885f4d2377986c059eca26a483f404
MikaelBraghini <mikael.braghini@gmail.com>
2024-11-13 17:07:32 -0300

    Correcoes de funcionamento

--- src/main/IntroducaoLocalDate.java
+++ src/main/IntroducaoLocalDate.java
@@ -2,6 +2,7 @@ package main;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class IntroducaoLocalDate {
     public static void main(String[] args) {
@@ -10,20 +11,20 @@ public class IntroducaoLocalDate {
         System.out.println("Data Atual: " + today);
 
         // Somando 10 dias na data atual
-        LocalDate datePlus10Days = today.plusDays(10);
-        System.out.println("Data após adicionar 10 dias: " + datePlus10Days);
+        LocalDate somandoDezDias = today.plusDays(10);
+        System.out.println("Data após adicionar 10 dias: " + somandoDezDias);
 
         // Subtraido 2 meses na data atual
-        LocalDate dateMinus2Months = today.minusMonths(2);
-        System.out.println("Data após subtrair 2 meses: " + dateMinus2Months);
+        LocalDate subtraindoDoisMeses = today.minusMonths(2);
+        System.out.println("Data após subtrair 2 meses: " + subtraindoDoisMeses);
 
         // Somando 1 ano na data atual
-        LocalDate dateNextYear = today.plusYears(1);
-        System.out.println("Data após adicionar 1 ano: " + dateNextYear);
+        LocalDate somandoUmAno = today.plusYears(1);
+        System.out.println("Data após adicionar 1 ano: " + somandoUmAno);
 
         // Formatando padrao de data
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        LocalDate dataConvertida = LocalDate.parse("31/10/2023", formatter);
+        DateTimeFormatter padrao = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate dataConvertida = LocalDate.parse("31/10/2023", padrao);
         System.out.println("\nData Convertida: " + dataConvertida);
 
     }
